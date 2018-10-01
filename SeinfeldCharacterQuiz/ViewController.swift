22e20d2fe0e5b521df578f9ae5f2968c494a77c6
Luke Phelan <luke.phelan@hotmail.com>
2018-10-02 06:44:53 +1000

    Initial Commit

---
+++ SeinfeldCharacterQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeinfeldCharacterQuiz
+//
+//  Created by Luke Phelan on 2/10/18.
+//  Copyright © 2018 Luke Phelan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
